dc745ff00fbcfc5863565951d8cf5b2f8bae834a
Kyrill Tircher <kyrilltircher14@hotmail.com>
2018-03-17 18:14:04 +0100

    env file

--- babel-server-start.js
+++ babel-server-start.js
@@ -1,4 +1,5 @@
 //setup ES6 environment
 require('babel-register');
 require('babel-polyfill');
+require('dotenv').config();
 require('./server-start');
--- server-start.js
+++ server-start.js
@@ -1,8 +1,3 @@
-//setup .env file setup
-import dotenv from 'dotenv';
-dotenv.config();
-//setup the logger for the server
-//import './server/modules/Logger';
 //start the server
 import './server';
 
