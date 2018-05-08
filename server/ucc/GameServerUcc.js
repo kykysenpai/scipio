5d598d1445206ffef5c012fb7667c1e30c0f6878
Kyrill Tircher <kyrilltircher14@hotmail.com>
2018-05-08 22:46:10 +0200

    use of Number() in port check

--- server/ucc/GameServerUcc.js
+++ server/ucc/GameServerUcc.js
@@ -2,15 +2,15 @@ import checkPort from "tcp-port-used";
 import Config from "../config/Config";
 
 const getStateMinecraft = async () => {
-    return await checkPort.check(Config.MINECRAFT_PORT, '127.0.0.1');
+    return await checkPort.check(Number(Config.MINECRAFT_PORT), '127.0.0.1');
 };
 
 const getStateTrackmania = async () => {
-    return await checkPort.check(Config.TRACKMANIA_PORT, '127.0.0.1');
+    return await checkPort.check(Number(Config.TRACKMANIA_PORT), '127.0.0.1');
 };
 
 const getStateConan = async () => {
-    return await checkPort.check(Config.CONAN_PORT,'127.0.0.1');
+    return await checkPort.check(Number(Config.CONAN_PORT),'127.0.0.1');
 };
 
 export default {getStateMinecraft, getStateTrackmania, getStateConan}
