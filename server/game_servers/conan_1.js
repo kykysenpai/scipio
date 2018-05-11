3c5f48ba0b4f2f84abc8d511772fb99d121370cc
Kyrill Tircher <kyrilltircher14@hotmail.com>
2018-05-11 17:53:24 +0200

    change in conan and minecraft server socket handlingZ

--- server/game_servers/conan_1.js
+++ server/game_servers/conan_1.js
@@ -1,76 +1,72 @@
-import Logger from "../modules/Logger";
 import Config from "../config/Config";
 import JWT from "jsonwebtoken";
 import Permissions from "../config/constants/Permissions";
 import child_process from "child_process";
-import GameServerUcc from "../ucc/GameServerUcc";
-import HttpStatus from "../config/constants/HttpStatus";
-import checkPort from "tcp-port-used";
 import Paths from "../config/constants/Paths";
 
 let spawn = child_process.spawn;
 
 const getServer = async (io, socket) => {
-    if(io.gameserver != null){
+    if (io.gameserver != null) {
         addSocketToListeningRoom(io.gameserver, socket);
     }
 };
 
-const stopServer = async (io) => {
-    if(io.gameserver != null){
-        Logger.info("Stopping Trackmania Server...");
+const stopServer = async (io, socket) => {
+    if (io.gameserver != null) {
+        socket.emit('stdout', {
+            data: "Stopping Conan_1 Server..."
+        });
         io.gameserver.kill('SIGINT');
     } else {
-        Logger.info("Can't stop the Trackmania Server as no instance is currently running");
+        socket.emit('stdout', {
+            data: "Can't stop the Conan_1 Server as no instance is currently running"
+        });
     }
 };
 
 const startServer = async (io, socket) => {
-    if(io.gameserver == null) {
-        // io.gameserver = spawn('/bin/bash', [Paths.GAME_SERVER_SCRIPTS + '/start_conan_1_server.sh']);
-        io.gameserver = spawn('/bin/bash', [Paths.GAME_SERVER_SCRIPTS + '/start_print_server.sh']);
+    if (io.gameserver == null) {
+        io.gameserver = spawn('/bin/bash', [Paths.GAME_SERVER_SCRIPTS + '/start_conan_1_server.sh']);
 
         socket.emit('stdout', {
             data: "The Conan_1 server is starting on tcc.tircher.be:" + Config.CONAN_PORT_1
         });
-
-        addSocketToListeningRoom(io.gameserver, socket);
-
-        io.gameserver.stdout.on('data', data => {
-            io.to(Config.GAMESERVER_ROOM).emit('stdout', {
-                data: data.toString()
-            });
-        });
-
-        io.gameserver.stderr.on('data', data => {
-            io.to(Config.GAMESERVER_ROOM).emit('stderr', {
-                data: data.toString()
-            });
-        });
-
-        io.gameserver.on('exit', () => {
-            io.to(Config.GAMESERVER_ROOM).emit('stdout', {
-                data: "The Conan_1 server was stopped"
-            });
-            delete io.gameserver;
-            Logger.info("The conan server is now closed");
-        });
     } else {
-        Logger.info("The Conan_1 couldn't start because another instance is already running");
         socket.emit('stdout', {
             data: "The Conan_1 server is already running on tcc.tircher.be:" + Config.CONAN_PORT_1
         })
     }
+
+    io.gameserver.stdout.on('data', data => {
+        io.to(Config.GAMESERVER_ROOM).emit('stdout', {
+            data: data.toString()
+        });
+    });
+
+    io.gameserver.stderr.on('data', data => {
+        io.to(Config.GAMESERVER_ROOM).emit('stderr', {
+            data: data.toString()
+        });
+    });
+
+    io.gameserver.on('exit', () => {
+        io.to(Config.GAMESERVER_ROOM).emit('stdout', {
+            data: "The Conan_1 server was stopped"
+        });
+        delete io.gameserver;
+    });
+
 };
 
-const addSocketToListeningRoom = (gameserver, socket) => {
+const addSocketToListeningRoom = (socket) => {
     socket.join(Config.GAMESERVER_ROOM);
 };
 
 const authenticate = async (socket, token) => {
     let decodedToken = await JWT.decode(token, Config.COOKIE_SECRET);
     let jwtPerms = decodedToken.permissions;
-    if(jwtPerms.includes(Permissions.CONAN)){
+    if (jwtPerms.includes(Permissions.CONAN)) {
         socket.authenticated = true;
     } else {
         socket.authenticated = false;
@@ -81,29 +77,29 @@ const initSocket = (io) => {
     io.on('connection', socket => {
 
         socket.on('authenticate', async (data) => {
-            if(data.token != null){
+            if (data.token != null) {
                 await authenticate(socket, data.token);
-                if(socket.authenticated){
+                if (socket.authenticated) {
                     await getServer(io, socket);
                 }
             }
         });
 
         socket.on('state', () => {
-           socket.emit('state', io.gameserver != null);
+            if (socket.authenticated) {
+                socket.emit('state', io.gameserver != null);
+            }
         });
 
         socket.on('start', async () => {
-            if(socket.authenticated){
-                Logger.info("Starting Conan_1 Server...");
+            if (socket.authenticated) {
                 await startServer(io, socket);
-                Logger.info("Conan_1 Server started on tcc.tircher.be:" + Config.CONAN_PORT_1);
             }
         });
 
         socket.on('stop', async () => {
-            if(socket.authenticated){
-                await stopServer(io);
+            if (socket.authenticated) {
+                await stopServer(io, socket);
             }
         });
 
--- server/game_servers/minecraft.js
+++ server/game_servers/minecraft.js
@@ -9,66 +9,67 @@ import child_process from "child_process";
 let spawn = child_process.spawn;
 
 const getServer = async (io, socket) => {
-    if(io.gameserver != null){
+    if (io.gameserver != null) {
         addSocketToListeningRoom(io.gameserver, socket);
     }
 };
 
-const stopServer = async (io) => {
+const stopServer = async (io, socket) => {
     let inUse = await GameServerUcc.getStateMinecraft();
-    if(io.gameserver != null && inUse){
-        Logger.info("Stopping Minecraft Server...");
+    if (io.gameserver != null && inUse) {
+        socket.emit('stdout', {
+            data: "Stopping Minecraft Server..."
+        });
         io.gameserver.stdin.write("stop\n");
     } else {
-        Logger.info("Can't stop the Minecraft Server as no instance is currently running");
+        socket.emit('stdout', {
+            data: "Can't stop the Minecraft Server as no instance is currently running"
+        });
     }
 };
 
 const startServer = async (io, socket) => {
-    if(io.gameserver == null) {
+    if (io.gameserver == null) {
         io.gameserver = spawn('/bin/bash', [Paths.GAME_SERVER_SCRIPTS + '/start_minecraft_server.sh']);
 
         socket.emit('stdout', {
             data: "The Minecraft server is starting on tcc.tircher.be:" + Config.MINECRAFT_PORT
         });
-
-        addSocketToListeningRoom(io.gameserver, socket);
-
-        io.gameserver.stdout.on('data', data => {
-            io.to(Config.GAMESERVER_ROOM).emit('stdout', {
-                data: data.toString()
-            });
-        });
-
-        io.gameserver.stderr.on('data', data => {
-            io.to(Config.GAMESERVER_ROOM).emit('stderr', {
-                data: data.toString()
-            });
-        });
-
-        io.gameserver.on('exit', () => {
-            io.to(Config.GAMESERVER_ROOM).emit('stdout', {
-                data: "The Minecraft server was stopped"
-            });
-            delete io.gameserver;
-            Logger.info("The minecraft server is now closed");
-        });
     } else {
-        Logger.info("The Minecraft couldn't start because another instance is already running");
         socket.emit('stdout', {
             data: "The Minecraft server is already running on tcc.tircher.be:" + Config.MINECRAFT_PORT
         })
     }
+
+    io.gameserver.stdout.on('data', data => {
+        io.to(Config.GAMESERVER_ROOM).emit('stdout', {
+            data: data.toString()
+        });
+    });
+
+    io.gameserver.stderr.on('data', data => {
+        io.to(Config.GAMESERVER_ROOM).emit('stderr', {
+            data: data.toString()
+        });
+    });
+
+    io.gameserver.on('exit', () => {
+        io.to(Config.GAMESERVER_ROOM).emit('stdout', {
+            data: "The Minecraft server was stopped"
+        });
+        delete io.gameserver;
+    });
+
 };
 
-const addSocketToListeningRoom = (gameserver, socket) => {
+const addSocketToListeningRoom = (socket) => {
     socket.join(Config.GAMESERVER_ROOM);
 };
 
 const authenticate = async (socket, token) => {
     let decodedToken = await JWT.decode(token, Config.COOKIE_SECRET);
     let jwtPerms = decodedToken.permissions;
-    if(jwtPerms.includes(Permissions.MINECRAFT)){
+    if (jwtPerms.includes(Permissions.MINECRAFT)) {
         socket.authenticated = true;
     } else {
         socket.authenticated = false;
@@ -79,24 +80,22 @@ const initSocket = (io) => {
     io.on('connection', socket => {
 
         socket.on('authenticate', async (data) => {
-            if(data.token != null){
+            if (data.token != null) {
                 await authenticate(socket, data.token);
-                if(socket.authenticated){
+                if (socket.authenticated) {
                     await getServer(io, socket);
                 }
             }
         });
 
         socket.on('start', async () => {
-            if(socket.authenticated){
-                Logger.info("Starting Minecraft Server...");
+            if (socket.authenticated) {
                 await startServer(io, socket);
-                Logger.info("Minecraft Server started on tcc.tircher.be:" + Config.MINECRAFT_PORT);
             }
         });
 
         socket.on('stop', async () => {
-            if(socket.authenticated){
+            if (socket.authenticated) {
                 await stopServer(io);
             }
         });
