3bccbadc9d0369384a8d280b3c9595ddb6212613
ktircher <kyrill.tircher@centran.be>
2018-08-14 12:52:45 +0200

    bcrypt version

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,12 +2,10 @@
 
 . $(dirname "$0")/env.sh
 
-echo ${PROJECT_ROOT}
-
 docker run --rm \
     --name scipio_build \
     -v ${PROJECT_ROOT}:/app \
     -w /app \
     -u $UID \
     docker.tircher.be/nap \
-    npm install
+    npm install --verbose
